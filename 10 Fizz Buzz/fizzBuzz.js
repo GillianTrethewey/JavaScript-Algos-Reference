9aab98d8f6936843132b1e18fa78e2f3440f28d4
Gillian Trethewey <gilliantrethewey100@gmail.com>
2022-10-28 10:35:28 -0400

    fizz buzz

---
+++ 10 Fizz Buzz/fizzBuzz.js
@@ -0,0 +1,26 @@
+//Given a number as an input, print out every integer
+//from 1 to that number. However, when the integer is
+//divisible by 3, print out “Fizz”; when it’s divisible by 5,
+//print out “Buzz”; when it’s divisible by both 3 and 5,
+//print out “Fizz Buzz”.
+
+const fizzBuzz = (n) => {
+  for (let num = 1; num <= n; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("Fizz Buzz");
+    } else if (num % 3 === 0) {
+      console.log("Fizz");
+    } else if (num % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(num);
+    }
+  }
+};
+
+// const fizzBuzz = (n) => {
+//   for (let num = 1; num <= n; num++) {
+//     console.log((num % 3 ? "" : "fizz") + (num % 5 ? "" : "buzz") || num);
+//   }
+// };
+fizzBuzz(25);
